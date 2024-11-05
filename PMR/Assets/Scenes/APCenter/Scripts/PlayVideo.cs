10537df43bc1e76a054ec1c67a913568059ee4e6
Arne <arnev.c.23@gmail.com>
2024-11-05 15:53:31 +0100

    get video player on screen trough script

---
+++ PMR/Assets/Scenes/APCenter/Scripts/PlayVideo.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayVideo : MonoBehaviour
+{
+    VideoPlayer player;
+    void Start()
+    {
+        player = GetComponent<VideoPlayer>();
+        if(player == null)
+        {
+            Debug.Log("no video player component found");
+        }
+    }
+}
