dc2db0a8380a4fdb602e7312bdd84e329780c721
Arne <arnev.c.23@gmail.com>
2024-11-05 14:28:20 +0100

    copy script for button

---
+++ PMR/Assets/Scenes/APCenter/Scripts/Button.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class Button : XRBaseInteractable
+{
+    private Outline outline;
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        hoverEntered.AddListener(OnHover);
+        hoverExited.AddListener(OnHoverExited);
+        selectEntered.AddListener(OnGrab);
+
+        outline = GetComponent<Outline>();
+        if (outline == null)
+        {
+            Debug.LogError("Outline component not found on the GameObject.");
+        }
+        else
+        {
+            outline.enabled = false;
+        }
+    }
+
+    protected override void OnDisable()
+    {
+        hoverEntered.RemoveListener(OnHover);
+        selectEntered.RemoveListener(OnGrab);
+        base.OnDisable();
+    }
+
+    private void OnHover(HoverEnterEventArgs args)
+    {
+        if (outline != null)
+        {
+            outline.enabled = true;
+        }
+    }
+    private void OnHoverExited(HoverExitEventArgs args)
+    {
+        if (outline != null)
+        {
+            outline.enabled = false;
+        }
+    }
+
+    private void OnGrab(SelectEnterEventArgs args)
+    {
+        Debug.Log("grabbed interactable");
+    }
+}
