869acdabc01950408bd9d081d5ed3859c3d955e0
Arne <arnev.c.23@gmail.com>
2024-11-12 22:01:03 +0100

    disable elevator button outline by default

--- PMR/Assets/Scenes/APCenter/Scripts/GameManager.cs
+++ PMR/Assets/Scenes/APCenter/Scripts/GameManager.cs
@@ -5,7 +5,14 @@ public class GameManager : MonoBehaviour
     public GameObject[] elevatorButtons;
     void Start()
     {
-        
+        foreach (GameObject button in elevatorButtons)
+        {
+            var outlineScript = button.GetComponent<Outline>();
+            if(outlineScript != null)
+            {
+                outlineScript.enabled = false;
+            }
+        }
     }
 
     void Update()
