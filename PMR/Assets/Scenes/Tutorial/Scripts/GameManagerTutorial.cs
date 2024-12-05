4b86716aa0a6678815d8761a67ab289d587c32df
Arne <arnev.c.23@gmail.com>
2024-12-05 09:32:36 +0100

    make sure that XR origin always gets initiated correctly on game start

--- PMR/Assets/Scenes/Tutorial/Scripts/GameManagerTutorial.cs
+++ PMR/Assets/Scenes/Tutorial/Scripts/GameManagerTutorial.cs
@@ -3,9 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class GameManagerTutorial : MonoBehaviour
 {
+    public GameObject XROrigin;
+    public GameObject HardCodedStartPosition;
     void Start()
     {
-        
+        if(XROrigin != null &&  HardCodedStartPosition != null)
+        {
+            XROrigin.transform.position = HardCodedStartPosition.transform.position;
+            XROrigin.transform.rotation = HardCodedStartPosition.transform.rotation;
+        }
     }
 
     void Update()
