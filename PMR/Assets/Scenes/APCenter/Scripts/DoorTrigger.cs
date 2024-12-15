f4e3b786d2ab12191da4421c1a4206922d2c315d
Arne <arnev.c.23@gmail.com>
2024-12-15 14:32:45 +0100

    toggle door panel when elevator is entered

--- PMR/Assets/Scenes/APCenter/Scripts/DoorTrigger.cs
+++ PMR/Assets/Scenes/APCenter/Scripts/DoorTrigger.cs
@@ -1,9 +1,23 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class DoorTrigger : MonoBehaviour
 {
+    public GameObject pannel;
+    private bool hitBefore = false;
+    void Start()
+    {
+        if(pannel == null)
+        {
+            Debug.LogError("panel to toggle in glass elevator is not assigned");
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"Trigger hit by: {other.gameObject.name}");
+        if(pannel != null && !hitBefore)
+        {
+            pannel.SetActive(true);
+        }
+        hitBefore = true;
     }
 }
