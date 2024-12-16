ec2e671c2cc9dc18189ad93595f81629642dafbd
Arne <arnev.c.23@gmail.com>
2024-12-16 09:15:51 +0100

    stop playing audio when elevator is entered

--- PMR/Assets/Scenes/APCenter/Scripts/DoorTrigger.cs
+++ PMR/Assets/Scenes/APCenter/Scripts/DoorTrigger.cs
@@ -5,11 +5,25 @@ public class DoorTrigger : MonoBehaviour
 {
     public GameObject pannel;
     private bool hitBefore = false;
+    public GameObject gameManagerObject;
+    private GameManager gameManager;
     void Start()
     {
         if(pannel == null)
         {
             Debug.LogError("panel to toggle in glass elevator is not assigned");
+        }
+        if(gameManagerObject == null)
+        {
+            Debug.LogError("game manager object is not assigned");
+        }
+        else
+        {
+            gameManager = gameManagerObject.GetComponent<GameManager>();
+            if(gameManager == null)
+            {
+                Debug.LogError("game manager script is not assigned");
+            }
         }
     }
     private void OnTriggerEnter(Collider other)
@@ -17,6 +31,10 @@ public class DoorTrigger : MonoBehaviour
         if(pannel != null && !hitBefore)
         {
             pannel.SetActive(true);
+        }
+        if(gameManager != null && !hitBefore)
+        {
+            gameManager.StopAudio();
         }
         hitBefore = true;
     }
